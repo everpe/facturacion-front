3b4cae04f86ad5e0ad30183c770179d973a1b040
everpe <ever.penab@autonoma.edu.co>
2024-05-14 08:58:45 -0500

    ajustes vistas

--- src/app/components/list-invoices/list-invoices.component.ts
+++ src/app/components/list-invoices/list-invoices.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
@@ -11,6 +11,8 @@ import { FacturaService } from '../../services/factura.service';
 import { InvoiceDto } from '../../Dto/FacturaDto';
 import { AddInvoiceComponent } from '../add-invoice/add-invoice.component';
 import { EditInvoiceComponent } from '../edit-invoice/edit-invoice.component';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 @Component({
   selector: 'app-list-invoices',
   standalone: true,
@@ -22,23 +24,52 @@ import { EditInvoiceComponent } from '../edit-invoice/edit-invoice.component';
     MatInputModule,
     MatFormFieldModule,
     MatDialogModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatPaginator
   ],
   templateUrl: './list-invoices.component.html',
   styleUrl: './list-invoices.component.css'
 })
 export class ListInvoicesComponent {
-  invoices: InvoiceDto[] = [];
+  displayedColumns: string[] = ['Number', 'City', 'NameProduct', 'Value', 'Actions'];
+  dataSource!: MatTableDataSource<InvoiceDto>;
+
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  invoices: InvoiceDto[] = [
+  ];
+
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
 
   constructor(private facturaService: FacturaService, public dialog: MatDialog) { }      
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.invoices);
     this.getListInvoices();
   }
 
   getListInvoices() {
     this.facturaService.getInvoices().subscribe(data => {
       this.invoices = data;
+      this.dataSource.data = this.invoices; 
       console.log(data);
     });
   }
@@ -75,7 +106,7 @@ export class ListInvoicesComponent {
   }
 
   deleteInvoice(id: number): void {
-    if (confirm("Are you sure you want to delete this invoice?")) {
+    if (confirm("Desea eliminar la factura?")) {
       this.facturaService.deleteInvoice(id).subscribe(() => {
         this.getListInvoices();
       });
